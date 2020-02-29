8bd320d639a3ff8abd55325a4aa20b18fe73befd
JoyDeRuijter <joyderuijter@gmail.com>
2020-02-29 21:44:59 +0100

    Add prefabs, start on Arrows and Swiping script

---
+++ UnityProject/GameJam/Assets/Scripts/Arrows.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class Arrows : MonoBehaviour
+{
+    public GameObject arrow;
+    public int Number;
+    public bool isRight;
+    public bool isLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public int RandomNumber(int min, int max)
+    {
+        System.Random random = new System.Random();
+        return random.Next(min, max);
+    }
+
+    public void CreateArrows()
+    {
+        Number = RandomNumber(0, 1);
+        if (Number == 0)
+        {
+            isLeft = true;
+            isRight = false;
+        }
+        else if (Number == 1)
+        {
+            isLeft = false;
+            isRight = true;
+        }
+
+        if (isLeft == true)
+        {
+            GameObject arrowClone = Instantiate(arrow, new Vector2((float)-2, (float)-0.5), Quaternion.identity);
+            //idk hoe ik hier in verder moet atm
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProject/GameJam/Assets/Scripts/Swiping.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swiping : MonoBehaviour
+{
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //if (visablearrow = leftarrow)
+                //delete arrowclone
+                // up score
+            //else if (visablearrow = rightarrow)
+                //show error sign
+                //delete arrowclone
+                // keep score or maybe even lose points
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //if (visablearrow = rightarrow)
+            //delete arrowclone
+            // up score
+            //else if (visablearrow = leftarrow)
+            //show error sign
+            //delete arrowclone
+            // keep score or maybe even lose points
+        }
+    }
+
+    public int CalculateScore()
+    {
+        // keep track of score
+        return score;
+    }
+
+    public void WriteScore()
+    {
+        // write score on the screen using CalculateScore(); and int score
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+    }
+}
