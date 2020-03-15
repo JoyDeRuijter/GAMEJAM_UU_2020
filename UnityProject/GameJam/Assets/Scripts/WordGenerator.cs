2bbbc0d628233d37aa00ae1ccb67c12a2d12ff20
Siwertdj <siwert@live.nl>
2020-03-15 16:26:45 +0100

    updated unigame sprites

--- UnityProject/GameJam/Assets/Scripts/WordGenerator.cs
+++ UnityProject/GameJam/Assets/Scripts/WordGenerator.cs
@@ -6,11 +6,13 @@ public class WordGenerator : MonoBehaviour
 {
     //    This script will be generating strings of random length and random symbols, and place them in the correct position within the book.
     //    The length of each word should be saved, so we know where the next word should go, and when to start a new line.
-    // TODO: Fix the position of instantiation
+    // TODO: Fix the position of instantiation.
+    //    Preferably, It should check whether the next word could still fit on the same line (withing the correct page)
+    //    If so, the words must be instantiated within 'hitboxes' of the pages. Is this possible???
 
     public GameObject word;
-    
-    // public static bool openMenu;
+    private Vector3 bookPosition;
+    private Vector3 wordPosition;
     
     public void Start()
     {
@@ -20,9 +22,9 @@ public class WordGenerator : MonoBehaviour
     
     public void CreateWords()
     {
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < 1; i++)
         {
-            GameObject wordClone = Instantiate(word, new Vector3(1 + i*1000000000000000, 1, 1), Quaternion.identity);
+            GameObject wordClone = Instantiate(word, transform.position, Quaternion.identity);
         }
     }
 }
