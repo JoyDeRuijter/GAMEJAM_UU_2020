4b0282ec8be93c06efa3b5ea4fc1dde342e62834
JoyDeRuijter <joyderuijter@gmail.com>
2020-03-18 12:43:21 +0100

    Add TinderGame

---
+++ UnityProject/GameJam/Assets/Scripts/Goal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+    public Text goalText;
+    public int goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goal = 300;
+        goalText.text = "Goal: " + goal;
+    }
+}
